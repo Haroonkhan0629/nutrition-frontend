11b33d4d945d77420127f2c3331cb9d4675cad48
Haroon Khan <hronkkhan6@gmail.com>
2024-03-04 15:56:57 -0800

    profile

--- nutrition-app/src/components/Home.js
+++ nutrition-app/src/components/Home.js
@@ -27,41 +27,81 @@ class Home extends Component {
 
   render() {
     const profile = this.props.profile
+    const theme = this.props.theme
     if (profile) {
-      return (
-        <div>
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item"><Link to="/">Home</Link></li>
-              <li class="breadcrumb-item active" aria-current="page">Exercises</li>
-            </ol>
-          </nav>
-          <ExerciseList
-            exercises={this.state.exercises}
-            profile={profile}
-            resetState={this.resetState}
-            theme={this.props.theme}
-          />
-          <NewExerciseModal create={true} resetState={this.resetState} />
-        </div>
-      );
+      if (theme === 'light') {
+        return (
+          <div>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><Link to="/">Home</Link></li>
+                <li class="breadcrumb-item active" aria-current="page">Exercises</li>
+              </ol>
+            </nav>
+            <ExerciseList
+              exercises={this.state.exercises}
+              profile={profile}
+              resetState={this.resetState}
+              theme={theme}
+            />
+            <NewExerciseModal create={true} resetState={this.resetState} />
+          </div>
+        );
+      } else if (theme === 'dark') {
+        return (
+          <div>
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><Link to="/">Home</Link></li>
+                <li aria-current="page">Exercises</li>
+              </ol>
+            </nav>
+            <ExerciseList
+              exercises={this.state.exercises}
+              profile={profile}
+              resetState={this.resetState}
+              theme={theme}
+            />
+            <NewExerciseModal create={true} resetState={this.resetState} />
+          </div>
+        );
+      }
     } else {
-      return (
-        <div className="table-margin">
-          <nav aria-label="breadcrumb">
-            <ol class="breadcrumb">
-              <li class="breadcrumb-item"><Link to="/">Home</Link></li>
-              <li aria-current="page">/ Exercises</li>
-            </ol>
-          </nav>
-          <ExerciseList
-            exercises={this.state.exercises}
-            profile={profile}
-            resetState={this.resetState}
-            theme={this.props.theme}
-          />
-        </div>
-      )
+      if (theme === 'light') {
+        return (
+          <div className="table-margin">
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><Link to="/">Home</Link></li>
+                <li class="breadcrumb-item active" aria-current="page">/ Exercises</li>
+              </ol>
+            </nav>
+            <ExerciseList
+              exercises={this.state.exercises}
+              profile={profile}
+              resetState={this.resetState}
+              theme={theme}
+            />
+          </div>
+        )
+      } else if (theme === 'dark') {
+        return (
+          <div className="table-margin">
+            <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item"><Link to="/">Home</Link></li>
+                <li aria-current="page">/ Exercises</li>
+              </ol>
+            </nav>
+            <ExerciseList
+              exercises={this.state.exercises}
+              profile={profile}
+              resetState={this.resetState}
+              theme={theme}
+            />
+          </div>
+        )
+      }
     }
   }
 }
